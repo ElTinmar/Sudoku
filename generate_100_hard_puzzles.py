7b1db8b8ce37fb7a9b4ac26bfdf115c39c5d0724
Tinmar <ElTinmar@users.noreply.github.com>
2022-08-31 15:21:02 +0200

    Add files via upload

--- generate_100_hard_puzzles.py
+++ generate_100_hard_puzzles.py
@@ -1,9 +1,9 @@
 from sudoku import Sudoku
 
 sdk = Sudoku()
-(clues,backtrack) = sdk.generate_puzzle()
+clues = sdk.generate_puzzle()
 for i in range(0,100):
     while clues >= 23:
-        (clues,backtrack) = self.generate_puzzle()
+        clues = self.generate_puzzle()
     filename = "hard" + str(i).zfill(2) + ".svg"
     sdk.export_grid(filename)
