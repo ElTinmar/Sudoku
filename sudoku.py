e4fc7a45b79dd00f000931ca628c91a32cf9bdd7
Tinmar <ElTinmar@users.noreply.github.com>
2022-09-01 11:42:44 +0200

    Add files via upload

--- sudoku.py
+++ sudoku.py
@@ -3,19 +3,32 @@
 
 import random
 
-class History:
+class Tree:
     '''Simple tree class'''
     def __init__(self, grid):
         self.children = []
         self.grid = grid
+        self.solution = False
     
     def add_child(self, node):
         self.children.append(node)
     
     def __repr__(self):
-        reprstr = ""
+        if self.solution:
+            reprstr = "\033[92m"
+        else:
+            reprstr = ""
+            
         for i in self.grid:
-            reprstr += str(i)
+            if i==0:
+                reprstr += "\033[91m" + str(i) + "\033[0m"
+            else:
+                reprstr += str(i)
+            
+        if self.solution:
+            reprstr += '\033[0m'
+            
+        reprstr += "\n"
         return reprstr
 
 # TODO fix problems with multiple digits for Sudoku(4) and beyond
@@ -33,15 +46,13 @@ class Sudoku:
         for i in range(0,self.tot_size):
             self.grid.append(0)
         self.visited = []
-        self.stuck = False
-        self.history = History(self.grid)
+        self.tree = Tree(self.grid)
         
     def reset_grid(self):
         self.visited = []
-        self.stuck = False
         for i in range(0,self.tot_size):
             self.grid[i] = 0
-        self.history = History(self.grid)
+        self.tree = Tree(self.grid)
         
     def prune_grid(self): 
         solution = self.grid[:]
@@ -53,19 +64,60 @@ class Sudoku:
                 prev_num = self.grid[idx]
                 self.grid[idx] = 0
                 previous_grid = self.grid[:]
-                self.history = History(self.grid)
+                self.tree = Tree(self.grid)
                 valid = self.solve() 
                 if not valid or not self.cmp_grid(solution):
                     not_prunable.add(idx)
                     self.grid = previous_grid
                     self.grid[idx] = prev_num
                 self.grid = previous_grid
-                self.history = History(self.grid)
+                self.tree = Tree(self.grid)
             else:
                 break
         self.visited = []
-        self.stuck = False
-
+        
+    def generate_puzzle(self):
+        self.generate_full_grid()
+        self.prune_grid()
+        return self.puzzle_level()
+        
+    def puzzle_level(self):
+        # number of clues
+        num_clues = len([i for i in self.grid if i != 0])
+        
+        # structure of the tree : check visited
+        
+        return num_clues
+        
+    def read_grid(self,filename):
+        self.reset_grid()
+        with open(filename,'r') as fin:
+            counter = 0
+            while True:
+                n = fin.read(1)
+                if n == '\n':
+                    break
+                if n == ' ':
+                    self.grid[counter] = 0
+                else:
+                    self.grid[counter] = int(n)
+                counter = counter+1
+            if counter != self.tot_size:
+                raise RuntimeError('wrong grid format')
+            else:
+                if not self.valid():
+                    raise RuntimeError('grid is invalid')
+                    
+        self.tree = Tree(self.grid) 
+        
+    def write_grid(self,filename):
+        with open(filename,'w') as fout:
+            for num in self.grid:
+                if num == 0:
+                    fout.write(' ')
+                else:
+                    fout.write(num)
+                    
     def export_grid(self,filename):
         """export grid as svg""" 
         svgstring = "<svg height='90' width='90'>\n"
@@ -128,136 +180,100 @@ class Sudoku:
         svgstring = svgstring + "</svg>"
         with open(filename,'w') as fout:
             fout.write(svgstring)
-            
+        
     def generate_full_grid(self):
-        # TODO not super satisfying, can I do better ?
-        # reset grid
-        self.reset_grid()
+        '''Use DFS to generate grid from an empty grid'''
         
-        while not self.complete():
-            # select most constrained cell
-            (r_sel,c_sel,candidates_sel) = self.get_most_constrained()
-        
-            if self.stuck:
-                 self.reset_grid()
-                 (r_sel,c_sel,candidates_sel) = self.get_most_constrained()
-                 
-            # pick at random among candidates
-            num = random.sample(candidates_sel,1)[0]
-            idx = self.sub2ind(r_sel,c_sel)
-            self.grid[idx] = num
+        def grow_tree():
+            (r,c,candidates) = self.get_most_constrained()
+            for num in candidates:
+                grid = self.grid[:]
+                idx = self.sub2ind(r,c)
+                grid[idx] = num
+                child = Tree(grid)
+                node.add_child(child)
+            random.shuffle(node.children)
             
-        self.history = History(self.grid) 
-        self.visited = []
-        self.stuck = False
-        
-    def generate_puzzle(self):
-        self.generate_full_grid()
-        self.prune_grid()
-        return self.puzzle_level()
-        
-    def read_grid(self,filename):
         self.reset_grid()
-        with open(filename,'r') as fin:
-            counter = 0
-            while True:
-                n = fin.read(1)
-                if n == '\n':
-                    break
-                if n == ' ':
-                    self.grid[counter] = 0
-                else:
-                    self.grid[counter] = int(n)
-                counter = counter+1
-            if counter != self.tot_size:
-                raise RuntimeError('wrong grid format')
-            else:
-                if not self.is_valid():
-                    raise RuntimeError('grid is invalid')
-                    
-        self.history = History(self.grid) 
-        
-    def puzzle_level(self):
-        # number of clues
-        num_clues = len([i for i in self.grid if i != 0])
-        
-        return num_clues
-        
-    def export_grid(self,filename):
-        """export grid as svg"""
-        pass
-    
-    def write_grid(self,filename):
-        with open(filename,'w') as fout:
-            for num in self.grid:
-                if num == 0:
-                    fout.write(' ')
-                else:
-                    fout.write(num)
-        
-    def complete(self):
-        if 0 in self.grid:
-            return False
-        else:
-            return True
-            
-    def next_move(self, node):
-        (r,c,candidates) = self.get_most_constrained()
-        for num in candidates:
-            grid = self.grid[:]
-            idx = self.sub2ind(r,c)
-            grid[idx] = num
-            child = History(grid)
-            node.add_child(child)
-        
-    def solve(self):
-        '''Use DFS to visit the tree of possible moves'''
-        
-        solution = []
-        unique = True
         stack = []
-        root = self.history
-        self.next_move(root)
+        root = self.tree
         stack.append(root)
         
         while len(stack)>0:
             node = stack.pop()
             if node not in self.visited:
                 self.grid = node.grid[:]
-                self.next_move(node)
+                grow_tree()
+                self.visited.append(node)
+                for c in node.children:
+                    stack.append(c)
+                    
+                if self.complete():
+                    break
+                    
+    def solve(self,display=False):
+        '''Use DFS to visit the tree of possible moves'''
+        
+        def grow_tree():
+            (r,c,candidates) = self.get_most_constrained()
+            for num in candidates:
+                grid = self.grid[:]
+                idx = self.sub2ind(r,c)
+                grid[idx] = num
+                child = Tree(grid)
+                node.add_child(child)
+            
+        sol = []
+        unique = True
+        stack = []
+        root = self.tree
+        stack.append(root)
+        
+        while len(stack)>0:
+            node = stack.pop()
+            if node not in self.visited:
+                self.grid = node.grid[:]
+                grow_tree()
                 self.visited.append(node)
                 for c in node.children:
                     stack.append(c)
                 
                 # check if we have solved the puzzle
                 if self.complete():
-                    if solution == []:
-                        solution = self.grid[:]
+                    node.solution = True
+                    if sol== []:
+                        sol = self.grid[:]
                     else:
-                        if not self.cmp_grid(solution):
+                        if not self.cmp_grid(sol):
                             unique = False
-                            break
+                
+                if display:
+                    print(node, end='')
+                
+                if not unique:
+                    break
         
-        self.grid = solution
+        self.grid = sol
         if self.complete() and unique:
             return True
         else:
             return False
-                    
+        
     def get_most_constrained(self):
         least_num_choice = self.max_num+1
         r_sel = 0
         c_sel = 0
         candidates_sel = []
-        for r in range(0,self.max_num):
-            for c in range(0,self.max_num):
-                candidates = self.get_uniq_candidate(r,c)
-                num_choice = len(candidates)
-                if num_choice > 0 and num_choice < least_num_choice:
-                    least_num_choice = num_choice
-                    r_sel = r
-                    c_sel = c
-                    candidates_sel = candidates
+        unknown = [i for i,v in enumerate(self.grid) if v == 0]
+        for idx in unknown:
+            (r,c) = self.ind2sub(idx)
+            candidates = self.get_uniq_candidate(r,c)
+            num_choice = len(candidates)
+            if num_choice > 0 and num_choice < least_num_choice:
+                least_num_choice = num_choice
+                r_sel = r
+                c_sel = c
+                candidates_sel = candidates
     
         return (r_sel,c_sel,candidates_sel)
             
@@ -305,8 +321,6 @@ class Sudoku:
             not_candidate = set(row+col+sqr)
             possible = {i for i in range(1,self.max_num+1)}
             candidate = list(possible - not_candidate)
-            if len(candidate)==0:
-                self.stuck = True
             return candidate
         else:
             return []
@@ -367,6 +381,12 @@ class Sudoku:
                     return False
         return True
         
+    def complete(self):
+        if 0 in self.grid:
+            return False
+        else:
+            return True
+            
     def __repr__(self):
         gridstr = "\n"
         for r in range(0,self.max_num):
